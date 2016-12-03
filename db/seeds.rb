3b01fc9674bc57dab86b8e6223e077218666b68b
TranHuuMinh <minhth1905@gmail.com>
2016-12-03 08:43:41 +0700

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+10.times do |i|
+  departments = Department.new(name: "Khoa #{i + 1}", office: "Văn phòng #{i + 1}")
+  departments.save
+  id = departments.id
+  10.times do |f|
+    Subject.create(name: "Bộ môn #{f + 1},  Khoa #{i + 1}", department_id: id)
+  end
+
+  10.times do |f|
+    Lab.create(name: "Phòng thí nghiệm #{f + 1},  Khoa #{i + 1}", department_id: id)
+  end
+end
