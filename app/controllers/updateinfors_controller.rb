6aaefff4831a8178d3554ffe71b57ee8d4e3978c
hieptm96 <hiep1996tb@gmail.com>
2016-12-07 16:20:50 +0700

    Add research and filter ajax

--- app/controllers/updateinfors_controller.rb
+++ app/controllers/updateinfors_controller.rb
@@ -1,12 +1,29 @@
 class UpdateinforsController < ApplicationController
   before_action :teacher
+  skip_before_action :verify_authenticity_token
+  def index
+  end
 
   def show
   end
 
   def new
+    @spheres = Sphere.all
+    @list_tree_spheres = show_tree(@spheres,0,0)
   end
 
   def create
+    @research = Research.new(name: params[:name], description: params[:description], teacher_id: current_user.teacher.id)
+    if @research.save
+      params[:sphere_array_id].each do |item|
+        Searchsphere.create(research_id: @research.id, sphere_id: item)
+      end
+      flash[:success] = "Thêm chủ đề hướng nghiên cứu thành công"
+      redirect_to updateinfors_path
+    else
+      flash[:danger] = "Thêm chủ đề hướng nghiên cứu thất bại"
+      render :new
+    end
+    # render text: params
   end
 end
