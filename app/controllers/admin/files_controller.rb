5659286ca43a66d8f846bc498d97b0804be8e1c9
TranHuuMinh <minhth1905@gmail.com>
2016-12-10 11:10:53 +0700

    add-file

--- app/controllers/admin/files_controller.rb
+++ app/controllers/admin/files_controller.rb
@@ -1,4 +1,4 @@
-class Admin::TimeProtectsController < ApplicationController
+class Admin::FilesController < ApplicationController
   before_action :user_signin
   before_action :department
   skip_before_action :verify_authenticity_token
@@ -7,32 +7,25 @@ class Admin::TimeProtectsController < ApplicationController
     @timenotifi = Timenotifi.find_by(department_id: current_user.departmentuser.department_id)
     if @timenotifi.blank?
       @check = 0
-    elsif @timenotifi.status < 2
-      @check = 0
-    else
-      @check = 1
     end
-    @subject = "Email thông báo về thời gian bảo vệ khóa luận"
-    @content = "Nhà trường thông báo đến toàn thể sinh viên về việc bảo vệ đề
+    @subject = "Email nhắc nhở về việc nộp hồ sơ khóa luận"
+    @content = "Nhà trường thông báo đến sinh viên để nhắc nhở về việc nộp hồ sơ đề
     tài khóa luận tốt nghiệp của sinh viên. Đề nghị sinh viên hoàn tất hồ sơ nộp cho khoa bộ môn"
 
-    @topics = Topic.where(status: 1, is_proteced: 1)
-
+    @topic = Topic.where(status: 1, is_proteced: 1)
+    @topics = Topic.where(status: 1, is_proteced: 2)
   end
 
   def create
     if params[:notifi]
-      @topics = Topic.where(status: 1, is_proteced: nil)
+      @topics = Topic.where(status: 1, is_proteced: 1)
       @students = []
-      @timenotifi = Timenotifi.find_by(department_id: current_user.departmentuser.department_id)
-      @timenotifi.update_attributes(status: 0)
       @topics.each do |topic|
-        topic.update_attributes(is_proteced: 1)
         @students << topic.student.user
       end
       if @students.length
         User.delay.send_email(@students, params[:subject], params[:content])
-        redirect_to admin_time_protects_path, notice: "Đang gửi email thông báo cho #{@students.length} sinh viên"
+        redirect_to admin_files_path, notice: "Đang gửi email thông báo cho #{@students.length} sinh viên"
       else
         flash[:danger] = "Không có sinh viên nào trong trạng thái được bảo vệ"
         render :new
