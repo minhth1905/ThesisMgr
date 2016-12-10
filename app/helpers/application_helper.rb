bc427e9394e9da1d1b847ed8e3bf94cbe7f89f91
hieptm96 <hiep1996tb@gmail.com>
2016-12-10 15:22:03 +0700

    Add announce notification

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
-  def info
-    return Info.where("user_id = ?", current_user.id).count
+  def get_info
+    return Info.where("user_receive = ? AND status = 1", current_user.id)
+  end
+
+  def get_user_send_name(id)
+    @user =  User.find_by(id: id)
+    return @user.first_name
   end
 end
---
+++ db/migrate/20161210055009_create_infos.rb
@@ -0,0 +1,15 @@
+class CreateInfos < ActiveRecord::Migration
+  def change
+    create_table :infos do |t|
+      t.text :content
+      t.integer :status
+      t.integer :user_send
+      t.integer :user_receive
+
+      t.timestamps null: false
+    end
+    add_index :infos, :user_send
+    add_index :infos, :user_receive
+    add_index :infos, [:user_send, :user_receive], unique: true
+  end
+end
